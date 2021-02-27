66698deba630751f075a04bfd13c332a5189525c
Wojciech Kluczka <wojciech.kluczka@gmail.com>
2021-02-28 00:13:09 +0700

    First commit

---
+++ edgin_around/src/main/kotlin/edgin_around/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.edgin.around
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+const val TAG: String = "EdginAround"
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
+
